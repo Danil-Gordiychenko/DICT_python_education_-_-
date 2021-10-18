8fea8dea2c8419ae1f07716e3287bf6aaf809c0a
Gordiychenko Danil <d.p.gordiychenko@student.khai.edu>
2021-10-18 11:11:53 +0300

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,6 +4,8 @@ print('The game will be available soon.')
 # Этап 2
 print('Do you want to start the game?')
 print('Start the game press 1; Exit press 2;')
+def restart(replay):
+    global start_game
 number3 = input()
 x=0
 while x != 1:
@@ -13,8 +15,8 @@ while x != 1:
        print("Start")
    else:
        print('HANGMAN')
-       print("Close")
-
+       print("Please, try again")
+       restart('')
 print('')
 print('HANGMAN')
 print('Guess the word:')
@@ -37,6 +39,32 @@ else:
     print("You lost!")
 # Этап 4
 import random
-a = random.choice(my_list)
+x = random.choice(my_list)
+
+print(
+    'HANGMAN')
+ln = len(x) - 3
+print('Guess the word:'+x[0:3]+"-"*ln)
+a = input()
+if a == x:
+    print("You survived!")
+else:
+    print("You lost!")
+
+# Этап 5
 print('HANGMAN')
-print('Guess the word',random.choice(my_list),':')
+import random
+my_list = ['python', 'java', 'javascript', 'php']
+a = random.choice(my_list)
+ln = len(a)
+print("-"*ln)
+print('Input a letter:')
+y = list(a)
+print(list(a))
+b = input()
+for c in [y]:
+ if b in c:
+    print("-" * ln)
+    print('Input a letter:')
+ else:
+     print("'That letter doesn't appear in the word'")
