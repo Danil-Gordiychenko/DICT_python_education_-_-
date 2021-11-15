9258248dcfb7b5f2198a046acb6b36af3fd472b7
Gordiychenko Danil <d.p.gordiychenko@student.khai.edu>
2021-11-15 10:09:58 +0200

    CreditCalculator.py

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,49 +1,80 @@
 import math
-def CreditCalculator():
-    print("What do you want to calculate?")
-    print('type "n" for number of monthly payments,')
-    print('type "a" for annuity monthly payment amount,')
-    print("type 'p' – for the monthly payment:")
-    x = input()
-    if x == 'a':
-        principal = int(input("Enter the loan principal:\n"))
-        p = principal
-        periods = int(input("Enter the number of periods:\n"))
-        n = periods
-        interest = int(input("Enter the loan interest:\n"))
-        i = interest / 1200
-        z = (i * ((1 + i) ** n))
-        y = (1 + i) ** n
-        a = (p * z) / (y - 1)
-        d = math.ceil(a)
-        print("Your monthly payment =", d, "!")
-    elif x == 'n':
-        principal = int(input("Enter the loan principal:\n"))
-        p = principal
-        payment = int(input("Enter the monthly payment:\n"))
-        a = payment
-        interest = int(input("Enter the loan interest:\n"))
-        i = interest / 1200
-        x = a/(a-(i*p))
-        degree = math.log(x, (1+i))
-        o = math.ceil(degree)
-        year = o / 12
-        round_up = round(year, (1))
-        r = str(round_up)
-        y = list(r)
-        print("It will take", y[0], "years and", y[2], "months to repay this loan!")
-    elif x == 'p':
-        a = float(input("Enter the annuity payment:\n"))
-        n = float(input("Enter the monthly payment:\n"))
-        interest = float(input("Enter the loan interest:\n"))
-        i = interest / 1200
-        b = i*(1+i)**n
-        c = (1+i)**n - 1
-        d = b/c
-        p = a/d
-        z = round(p)
-        print("Your monthly payment =", z, "!")
+import sys
+try:
+    args = sys.argv
+    type = args[1]
+    if type == "--type=diff":
+        p = args[2]
+        if p[:12] == "--principal=":
+            p = int(p[12:])
+            n = args[3]
+            if n[:10] == "--periods=":
+                n = int(n[10:])
+                i_percent = args[4]
+                if i_percent[:11] == "--interest=":
+                    i_percent = float(i_percent[11:])
+                    i = i_percent / (12 * 100)
+                    m = 1
+                    D = 0
+                    while m <= n:
+                        d = (p/n) + (i*(p - (p*(m-1)/n)))
+                        print("Month {}: paid out {}".format(m, math.ceil(d)))
+                        m += 1
+                        D += math.ceil(d)
+                    print("Overpayment = {}".format(D - p))
+                else:
+                    print("Incorrect Parameters")
+            else:
+                print("Incorrect Parameters")
+        else:
+            print("Incorrect Parameters")
+    elif type == '--type=annuity':
+        p = args[2]
+        if p[:12] == '--principal=':
+            p = int(p[12:])
+            a = args[3]
+            if a[:10] == '--payment=':
+                a = int(a[10:])
+                interest = args[4]
+                if interest[:11] == '--interest=':
+                    interest = float(interest[11:])
+                    i = interest / 1200
+                    x = a / (a - (i * p))
+                    degree = math.log(x, (1 + i))
+                    o = math.ceil(degree)
+                    year = o / 12
+                    round_up = round(year, (1))
+                    r = str(round_up)
+                    y = list(r)
+                    print("It will take", y[0], "years and", y[2], "months to repay this loan!")
+                else:
+                    print("Incorrect Parameters")
+            else:
+                print("Incorrect Parameters")
+        elif p[:10] == '--payment=':
+            p = float(p[10:])
+            n = args[3]
+            if n[:10] == '--periods=':
+                n = int(n[10:])
+                interest = args[4]
+                if interest[:11] == '--interest=':
+                    interest = float(interest[11:])
+                    i = interest / 1200
+                    b = i * (1 + i) ** n
+                    c = (1 + i) ** n - 1
+                    d = b / c
+                    x = p / d
+                    z = round(x)
+                    print("Your monthly payment =", z, "!")
+                else:
+                    print("Incorrect Parameters")
+            else:
+                print("Incorrect Parameters")
+        else:
+            print("Incorrect Parameters")
     else:
-        print("Pleas try again")
+        print("Incorrect Parameters")
 
-CreditCalculator()
+
+except Exception:
+    print("Incorrect Parameters")
