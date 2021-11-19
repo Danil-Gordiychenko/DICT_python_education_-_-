474cb3c279ea28299b8772bc0ba37f4e299d02f8
Gordiychenko Danil <d.p.gordiychenko@student.khai.edu>
2021-11-19 12:28:52 +0200

    DinnerParty.py

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,3 +1,4 @@
+import random
 y = int(input('Enter the number of friends joining (including you):\n'))
 x = range(y)
 buy = {}
@@ -13,7 +14,21 @@ while True:
             buy[name] = 0
         number = int(input('Enter the total amount:\n'))
         li = number / y
-        for i in x:
-            buy = {name: li for name in buy}
-        print(buy)
-        break
+        g = round(li, 2)
+        buy = {name: g for name in buy}
+        while True:
+            question = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+            if question == 'Yes':
+                v = random.choice(list(buy.keys()))
+                print(v, 'is the lucky one!')
+                li = number / (y-1)
+                g = round(li, 2)
+                buy = {name: g for name in buy}
+                buy[v] = 0
+                print(buy)
+            else:
+                print('No one is going to be lucky')
+                buy = {name: g for name in buy}
+                print(buy)
+                break
+    break
