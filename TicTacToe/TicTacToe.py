4b7e71ad202476240eb3bf985afee9a077ca91d6
Gordiychenko Danil <d.p.gordiychenko@student.khai.edu>
2021-11-29 00:04:26 +0200

    Almost the end

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -57,72 +57,73 @@ game_code()
 
 
 while True:
-    z = input('Enter the coordinates:')
-    s = list(z)
-    xi = int(z[0])
-    li = int(z[2])
-    if xi or li == int:
-        if 0 < xi <= 3 and 0 < li <= 3:
-            if variable == "X":
-                variable = "O"
-            else:
-                variable = "X"
-            if z == '1 1':
-                if list_new[0] != 'X' and list_new[0] != 'O':
-                    list_new[0] = variable
+    try:
+        z = input('Enter the coordinates:')
+        s = list(z)
+        xi = int(z[0])
+        li = int(z[2])
+        if xi or li == int:
+            if 0 < xi <= 3 and 0 < li <= 3:
+                if variable == "X":
+                    variable = "O"
                 else:
-                    print('This cell is occupied! Choose another one!')
-                    if variable == "X":
-                        variable = "O"
+                    variable = "X"
+                if z == '1 1':
+                    if list_new[0] != 'X' and list_new[0] != 'O':
+                        list_new[0] = variable
                     else:
-                        variable = "X"
-            elif z == '1 2':
-                if list_new[1] != 'X' and list_new[1] != 'O':
-                    list_new[1] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '1 3':
-                if list_new[2] != 'X' and list_new[2] != 'O':
-                    list_new[2] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '2 1':
-                if list_new[3] != 'X' and list_new[3] != 'O':
-                    list_new[3] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '2 2':
-                if list_new[4] != 'X' and list_new[4] != 'O':
-                    list_new[4] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '2 3':
-                if list_new[5] != 'X' and list_new[5] != 'O':
-                    list_new[5] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '3 1':
-                if list_new[6] != 'X' and list_new[6] != 'O':
-                    list_new[6] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '3 2':
-                if list_new[7] != 'X' and list_new[7] != 'O':
-                    list_new[7] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-            elif z == '3 3':
-                if list_new[8] != 'X' and list_new[8] != 'O':
-                    list_new[8] = variable
-                else:
-                    print('This cell is occupied! Choose another one!')
-        else:
-            print('Coordinates should be from 1 to 3!')
-    elif xi or li != int:
+                        print('This cell is occupied! Choose another one!')
+                        if variable == "X":
+                            variable = "O"
+                        else:
+                            variable = "X"
+                elif z == '1 2':
+                    if list_new[1] != 'X' and list_new[1] != 'O':
+                        list_new[1] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '1 3':
+                    if list_new[2] != 'X' and list_new[2] != 'O':
+                        list_new[2] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '2 1':
+                    if list_new[3] != 'X' and list_new[3] != 'O':
+                        list_new[3] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '2 2':
+                    if list_new[4] != 'X' and list_new[4] != 'O':
+                        list_new[4] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '2 3':
+                    if list_new[5] != 'X' and list_new[5] != 'O':
+                        list_new[5] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '3 1':
+                    if list_new[6] != 'X' and list_new[6] != 'O':
+                        list_new[6] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '3 2':
+                    if list_new[7] != 'X' and list_new[7] != 'O':
+                        list_new[7] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                elif z == '3 3':
+                    if list_new[8] != 'X' and list_new[8] != 'O':
+                        list_new[8] = variable
+                    else:
+                        print('This cell is occupied! Choose another one!')
+                play()
+                game_code()
+            else:
+                print('Coordinates should be from 1 to 3!')
+        elif xi or li != int:
+            print('You should enter numbers!')
+        if victory >= 1:
+            break
+    except ValueError:
         print('You should enter numbers!')
-    if victory >= 1:
-        break
-    play()
-    game_code()
-
-
