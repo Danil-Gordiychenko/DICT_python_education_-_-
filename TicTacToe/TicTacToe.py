367dc1c014c066809cabb517274ab0d82fc85621
Gordiychenko Danil <d.p.gordiychenko@student.khai.edu>
2021-12-01 22:31:16 +0200

    Almost the end

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -3,6 +3,86 @@ victory = 0
 variable = "X"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def play():
     print("---------")
     print("| ", end="")
@@ -55,68 +135,72 @@ def game_code():
 
 game_code()
 
-
 while True:
     try:
         z = input('Enter the coordinates:')
         s = list(z)
         xi = int(z[0])
         li = int(z[2])
+        isIncorrect = False
         if xi or li == int:
             if 0 < xi <= 3 and 0 < li <= 3:
-                if variable == "X":
-                    variable = "O"
-                else:
-                    variable = "X"
                 if z == '1 1':
                     if list_new[0] != 'X' and list_new[0] != 'O':
                         list_new[0] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
-                        if variable == "X":
-                            variable = "O"
-                        else:
-                            variable = "X"
                 elif z == '1 2':
                     if list_new[1] != 'X' and list_new[1] != 'O':
                         list_new[1] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '1 3':
                     if list_new[2] != 'X' and list_new[2] != 'O':
                         list_new[2] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '2 1':
                     if list_new[3] != 'X' and list_new[3] != 'O':
                         list_new[3] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '2 2':
                     if list_new[4] != 'X' and list_new[4] != 'O':
                         list_new[4] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '2 3':
                     if list_new[5] != 'X' and list_new[5] != 'O':
                         list_new[5] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '3 1':
                     if list_new[6] != 'X' and list_new[6] != 'O':
                         list_new[6] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '3 2':
                     if list_new[7] != 'X' and list_new[7] != 'O':
                         list_new[7] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
                 elif z == '3 3':
                     if list_new[8] != 'X' and list_new[8] != 'O':
                         list_new[8] = variable
                     else:
+                        isIncorrect = True
                         print('This cell is occupied! Choose another one!')
+
+                if not isIncorrect:
+                    variable = "O" if variable == "X" else "X"
                 play()
                 game_code()
             else:
